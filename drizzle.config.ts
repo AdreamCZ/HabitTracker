8c9c6ce4b1f3d814440590f80e3e9f485f5fefae
AdreamCZ <aramde@seznam.cz>
2025-12-10 15:16:23 +0100

    feat: switch to turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,9 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./lib/db/schema",
   out: "./drizzle",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
-    token: process.env.DATABASE_AUTH_TOKEN,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   },
 });
